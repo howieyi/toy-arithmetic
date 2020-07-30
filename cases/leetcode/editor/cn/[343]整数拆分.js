008d8ab1229cb1e66c74f5f81e3db86ffa9d7dd5
huyi <huyi247767221@163.com>
2020-07-31 05:33:49 +0800

    【343】整数拆分

---
+++ cases/leetcode/editor/cn/[343]整数拆分.js
@@ -0,0 +1,37 @@
+//给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+//
+// 示例 1:
+//
+// 输入: 2
+//输出: 1
+//解释: 2 = 1 + 1, 1 × 1 = 1。
+//
+// 示例 2:
+//
+// 输入: 10
+//输出: 36
+//解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+//
+// 说明: 你可以假设 n 不小于 2 且不大于 58。
+// Related Topics 数学 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function (n) {
+  var dp = [1, 1, 1];
+
+  for (var i = 2; i <= n; i++) {
+    var max = 0;
+    for (var j = 1; j < i; j++) {
+      max = Math.max(max, Math.max(j * (i - j), j * dp[i - j]))
+    }
+    dp[i] = max;
+  }
+
+  return dp[n];
+};
+//leetcode submit region end(Prohibit modification and deletion)
