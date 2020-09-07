b3f7fc8d4d8c65123f1da93d4150ccdf6dfafc19
huyi <huyi247767221@163.com>
2020-09-07 16:28:11 +0800

    [347]前 K 个高频元素

---
+++ cases/leetcode/editor/cn/[347]前 K 个高频元素.js
@@ -0,0 +1,62 @@
+//给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+//
+//
+//
+// 示例 1:
+//
+// 输入: nums = [1,1,1,2,2,3], k = 2
+//输出: [1,2]
+//
+//
+// 示例 2:
+//
+// 输入: nums = [1], k = 1
+//输出: [1]
+//
+//
+//
+// 提示：
+//
+//
+// 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+// 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+// 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+// 你可以按任意顺序返回答案。
+//
+// Related Topics 堆 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  if (!nums || !nums.length || !k) return [];
+
+  var map = {};
+
+  for (var num of nums) {
+    map[num] = map[num] || 0;
+    map[num]++;
+  }
+
+  var arr = [];
+  for (var prop in map) {
+    arr[map[prop]] = arr[map[prop]] || [];
+    arr[map[prop]].push(+prop);
+  }
+
+  var buffer = [];
+  for (var i = arr.length - 1; i >= 0 && buffer.length < k; i--) {
+    if (arr[i] === undefined) continue;
+
+    buffer = buffer.concat(arr[i]);
+  }
+
+  return buffer;
+};
+
+// console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+//leetcode submit region end(Prohibit modification and deletion)
