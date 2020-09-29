f348fb76b948da938326f925de1a2cd008e4da8c
huyi <huyi247767221@163.com>
2020-09-29 14:15:31 +0800

    [145]二叉树的后序遍历

---
+++ cases/leetcode/editor/cn/[145]二叉树的后序遍历.js
@@ -0,0 +1,51 @@
+//给定一个二叉树，返回它的 后序 遍历。
+//
+// 示例:
+//
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//输出: [3,2,1]
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树
+// 👍 435 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  var result = [];
+  var dfs = function (node) {
+    if (!node) return;
+
+    // 先左
+    node.left && dfs(node.left);
+
+    // 再右
+    node.right && dfs(node.right);
+
+    // 再根节点
+    result.push(node.val);
+  };
+
+  dfs(root);
+
+  return result;
+};
+//leetcode submit region end(Prohibit modification and deletion)
