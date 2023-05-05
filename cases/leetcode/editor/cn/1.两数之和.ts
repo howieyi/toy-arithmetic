24a959cdc1f2ed3919c6c87b66bf12d9bff8f88a
胡毅 <huyi2@fosunhealth.com>
2023-05-05 15:19:39 +0800

    feat: 日常打卡

---
+++ cases/leetcode/editor/cn/1.两数之和.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    if (!nums.length) return [];
+
+    const len = nums.length;
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+
+    return [];
+};
+// @lc code=end
+
